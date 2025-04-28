bec2d72fb11466068ef7e524cbb2534bf2e8de39
JASL22 <alexandersjhonn@gmail.com>
2025-04-28 17:26:51 -0500

    Create notebook

---
+++ Hola mundo.py
@@ -0,0 +1,4 @@
+a = int (input("Introduce un número: "))
+b = int (input("Introduce otro número: "))
+resultado = a + b
+print("El resultado de la suma es: ", resultado)
---
+++ Primero.py
@@ -0,0 +1,6 @@
+print  ('hola mundo')
+print('Es un progrma de python')
+num1 = 5
+num2 = 10
+suma = num1 + num2  
+print('La suma de', num1, 'y', num2, 'es:', suma)
